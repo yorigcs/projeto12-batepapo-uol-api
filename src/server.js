49235aa6f5ba231f28cd0cde4a0e49656193302e
Yorí <yorigcsdev@gmail.com>
2022-06-24 19:12:56 -0300

    fix: change logic MongoDB connection & fixes route get /messages

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dayjs from 'dayjs';
 import isValid from './validations.js';
-import { MongoConnect, MongoClose } from './MongoDB.js';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let mongoDB = null;
+mongoClient.connect()
+    .then(() => mongoDB = mongoClient.db(process.env.MONGO_DB))
+    .catch(err => console.log(err));
 
 const server = express();
 server.use([cors(), express.json()]);
@@ -19,10 +24,8 @@ server.post('/participants', async (req, res) => {
             res.sendStatus(422)
             return;
         }
-
-        const connect = await MongoConnect();
         //register new participant if not already registered
-        const participants = connect.collection(process.env.MONGO_COLLECTION_PARTICIPANTS);
+        const participants = mongoDB.collection("participants");
         const isParticipant = await participants.findOne({ name });
         if (isParticipant) {
             console.log('O nome já existe!')
@@ -32,32 +35,24 @@ server.post('/participants', async (req, res) => {
         await participants.insertOne({ name, lastStatus: Date.now() });
 
         //register new message type status from new participant
-        const messages = connect.collection(process.env.MONGO_COLLECTION_MESSAGES);
+        const messages = mongoDB.collection("messages");
         const messageStatus = { from: name, to: 'Todos', text: 'Entra na sala...', type: 'status', time: dayjs().format('HH:MM:ss') }
         await messages.insertOne(messageStatus);
-
         res.sendStatus(201);
-
     } catch (err) {
         console.log(err)
-        res.sendStatus(400);
-    } finally {
-        await MongoClose();
+        res.sendStatus(500);
     }
-
 });
 
 server.get('/participants', async (req, res) => {
     try {
-        const connect = await MongoConnect();
-        const participants = connect.collection(process.env.MONGO_COLLECTION_PARTICIPANTS);
+        const participants = mongoDB.collection("participants");
         const listParticipants = await participants.find({}).toArray();
         res.send(listParticipants);
     } catch (err) {
         console.log(err)
-        res.sendStatus(400);
-    } finally {
-        await MongoClose();
+        res.sendStatus(500);
     }
 })
 
@@ -65,11 +60,9 @@ server.post('/messages', async (req, res) => {
     const { to, text, type } = req.body;
     const from = req.header('User');
     try {
-
         const isValidMessage = await isValid({ to, text, type });
 
-        const connect = await MongoConnect();
-        const participants = connect.collection(process.env.MONGO_COLLECTION_PARTICIPANTS);
+        const participants = mongoDB.collection("participants");
         const isParticipant = await participants.findOne({ name: from });
 
         if (!isValidMessage || !isParticipant) {
@@ -77,36 +70,54 @@ server.post('/messages', async (req, res) => {
             return;
         }
 
-        const messages = connect.collection(process.env.MONGO_COLLECTION_MESSAGES);
+        const messages = mongoDB.collection("messages");
         await messages.insertOne({ from, to, text, type, time: dayjs().format('HH:MM:ss') })
-
         res.sendStatus(201);
-
     } catch (err) {
         console.log(err)
-        res.sendStatus(400);
+        res.sendStatus(500);
+    }
+});
 
-    } finally {
-        await MongoClose();
+server.get('/messages', async (req, res) => {
+    const limit = Number(req.query.limit);
+    const from = req.header('User');
+    if (!from) {
+        res.sendStatus(422);
+        return;
+    }
+    try {
+        const messages = mongoDB.collection("messages");
+        const showMessages = limit ? await messages.find({ $or: [{ from }, { to: from }, { to: 'Todos' }] }).sort({$natural: -1}).limit(limit).toArray()
+                            : await messages.find({ $or: [{ from }, { to: from }, { to: 'Todos' }] }).sort({$natural: -1}).toArray();
+        res.send(showMessages.reverse());
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500);
     }
 
 });
 
-server.get('/messages', async (req, res) => {
-    const limit = req.query.limit;
-    const from = req.header('User');
-
+server.post('/status', async (req, res) => {
+    const name = req.header('User');
+    if (!name) {
+        res.sendStatus(422);
+        return;
+    }
     try {
-        const connect = await MongoConnect();
-        const messages = connect.collection(process.env.MONGO_COLLECTION_MESSAGES);
-        const showMessages = limit ? await messages.find({ $or: [{ from }, { to: from }, {to: 'Todos'}] }).limit(Number(limit)).toArray()
-                                   : await messages.find({ $or: [{ from }, { to: from },{to: 'Todos'}] }).toArray();    
-        res.send(showMessages);
+        const participants = mongoDB.collection("participants");
+        const isParticipant = await participants.findOne({ name });
+        if (!isParticipant) {
+            res.sendStatus(404);
+            return;
+        }
+
+        await participants.updateOne({ name }, { $set: { lastStatus: Date.now() } });
+        res.sendStatus(200);
     } catch (err) {
         console.log(err)
-        res.sendStatus(400);
-    } finally {
-        MongoClose();
+        res.sendStatus(500);
+
     }
 
 });
