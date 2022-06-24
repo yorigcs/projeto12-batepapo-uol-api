105d2c5c92f64e9fb2587e81c4585df2d7731836
Yorí <yorigcsdev@gmail.com>
2022-06-23 22:19:37 -0300

    fix: MongoDB logic improvement

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dayjs from 'dayjs';
 import validations from './validations.js';
-import Mongo from './MongoDB.js';
+import { MongoConnect, MongoClose } from './MongoDB.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -13,24 +13,46 @@ server.use([cors(), express.json()]);
 server.post('/participants', async (req, res) => {
     const { name } = req.body;
     try {
-        await validations({username: name});
+        //is name valid?
+        await validations({ username: name });
 
-        const participant = { name, lastStatus: Date.now()};
-        await Mongo(process.env.MONGO_DB_NAME,process.env.MONGO_COLLECTION_PARTICIPANTS,participant,1);
+        const connect = await MongoConnect();
+        //register new participant if not already registered
+        const participants = connect.collection(process.env.MONGO_COLLECTION_PARTICIPANTS);
+        const isParticipant = await participants.findOne({ name });
+        if (isParticipant) {
+            console.log('O nome já existe!')
+            res.sendStatus(409)
+            return;
+        }
+        await participants.insertOne({ name, lastStatus: Date.now() });
 
-        const userLobby = {from: name, to: 'Todos', text: 'Entra na sala...', type: 'status', time: dayjs().format('HH:MM:ss')}
-        await Mongo(process.env.MONGO_DB_NAME,process.env.MONGO_COLLECTION_MESSAGES,userLobby,2)
-        
+        //register new message type status from new participant
+        const messages = connect.collection(process.env.MONGO_COLLECTION_MESSAGES);
+        const messageStatus = { from: name, to: 'Todos', text: 'Entra na sala...', type: 'status', time: dayjs().format('HH:MM:ss') }
+        await messages.insertOne(messageStatus);
+
         res.sendStatus(201);
+
     } catch (err) {
         console.log(err)
         res.sendStatus(err);
+    } finally {
+        await MongoClose();
     }
-    
-   
-
 
 });
+
+server.get('/participants', async (req, res) => {
+    try {
+        const connect = await MongoConnect();
+        const participants = connect.collection(process.env.MONGO_COLLECTION_PARTICIPANTS);
+        const listParticipants = await participants.find({}).toArray();
+        res.send(listParticipants);
+    } catch (err) {
+
+    }
+})
 
 server.listen(process.env.PORT, () => console.log('listening on port ' + process.env.PORT));
 
